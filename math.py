bc88fff8d6d6a2823df7775e00f09533db03b456
Peter Seibel <peter@gigamonkeys.com>
2017-11-23 09:33:16 -0800

    Some more refactoring of math grammar.

--- math.py
+++ math.py
@@ -16,16 +16,18 @@ if __name__ == '__main__':
         ws = star(match(str.isspace))
         return ws.then(literal(s)).then(ws).returning(lambda r: r[1])
 
+    def binary(left, op, right):
+        return match(left).then(optional(token(op).then(right))).returning(combine)
+
     p = {
-        'expression':    match('term').then(optional(token('+').then('expression'))).returning(combine),
-        'parenthesized': token('(').then('expression').then(token(')')),
+        'expression':    binary('term', '+', 'expression'),
+        'parenthesized': token('(').then('expression').then(token(')')).returning(lambda r: r[1]),
         'factor':        choice('parenthesized', 'number'),
-        'term':          match('factor').then(optional(token('*').then('term'))).returning(combine),
-        'digit':         match(str.isdigit),
-        'number':        text(star('digit')).returning(int),
+        'term':          binary('factor', '*', 'term'),
+        'number':        text(star(str.isdigit)).returning(int),
     }
 
-    input = TextInput('1234 + 4567 * 9876')
+    input = TextInput('1234 + 4567 * 9876 * (23 + 45)')
 
     ok, _, r = parse(p, 'expression', input)
     print(r if ok else 'Parse failed')
