09a445227bf358c1220a7708fc7c21bbb6175deb
Peter Seibel <peter@gigamonkeys.com>
2017-11-26 10:04:57 -0800

    Rewrite matcher tree in math2

--- math2.py
+++ math2.py
@@ -11,7 +11,17 @@ def combine(args):
     else:
         return first
 
-g = grammar('math.g')
+
+class TokenRewriter(Visitor):
+
+    def visit_string_matcher(self, matcher):
+        if matcher.expr in '+*()':
+            return TokenMatcher(matcher, regex('\\s'))
+
+
+visitor = TokenRewriter()
+
+g = { name : rule.accept(visitor) for name, rule in grammar('math.g').items() }
 
 g['expression']    = g['expression'].returning(combine)
 g['parenthesized'] = g['parenthesized'].returning(1)
@@ -20,7 +30,7 @@ g['number']        = g['number'].text(int)
 
 if __name__ == '__main__':
 
-    input = TextInput('1234+4567*9876*(23+45)')
+    input = TextInput('1234 + 4567 * 9876 * ( 23 + 45 )')
 
     ok, _, r = parse(g, 'expression', input)
     print(r if ok else 'Parse failed')
